58066383fafc155e29f7cfd677b84a2bf20a970b
Luina Borah <luina.borah@publicissapient.com>
2020-10-19 20:30:13 +0530

    Demo JPService1 to check its health for HealthCheck Service

---
+++ src/main/java/com/jpservice1/jpservice1/JpService1Application.java
@@ -0,0 +1,14 @@
+package com.jpservice1.jpservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class JpService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpService1Application.class, args);
+	}
+
+}
